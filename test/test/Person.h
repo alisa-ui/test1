74a1b4212e0cc7bfee0da41dce4197f8655d8359
zay <2405619307@qq.com>
2015-12-15 10:03:24 +0800

    2.做一些测试操作

---
+++ test/test/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  test
+//
+//  Created by zay on 15/12/15.
+//  Copyright © 2015年 zay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ test/test/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  test
+//
+//  Created by zay on 15/12/15.
+//  Copyright © 2015年 zay. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
